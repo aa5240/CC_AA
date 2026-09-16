7e379895b19528415ec567947a0e86a4f3f1eb52
aa5240 <aa5240@nyu.edu>
2026-09-16 18:53:53 -0400

    Update sketch.js

--- week3/p5PolarTemplate/sketch.js
+++ week3/p5PolarTemplate/sketch.js
@@ -5,10 +5,18 @@
 
 function setup() {
   createCanvas(500, 500);
-}
+  for (let i = 0; i < 360; i += 10) 
+    ellipse(x + width/2, y + height/2, i, 10);
+    ellipse(x + width/2, y + height/2, 10, i);
+    ellipse(x + width/2, y + height/2, 10, i);
+  }
 
 function draw() {
   background(255,0,0);
   text("p5.Polar Template", 100, height/2);
   // Insert your drawing here
+  // for loop - a piece of code
+  // until the condition is met
 }
+
+
