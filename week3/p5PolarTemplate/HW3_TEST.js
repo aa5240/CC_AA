57aec3ec1862abc35ff23e5b48b361bc56e30f35
aa5240 <aa5240@nyu.edu>
2026-09-16 19:55:55 -0400

    splash 7:55 PM, 9/16/2026

--- week3/p5PolarTemplate/HW3_TEST.js
+++ week3/p5PolarTemplate/HW3_TEST.js
@@ -1,34 +1,61 @@
 // p5Polar Template
 // https://github.com/liz-peng/p5.Polar
 // https://liz-peng.github.io/p5.Polar/
-// Review the index.html for the <script></script> 
+// Review the index.html for the <script></script>
 // my first p5.Polar sketch
+
 function setup() {
-  noloop();
-  createCanvas(1000, 500);
-  for (let i = 0; i < 360; i += 10) 
-    ellipse(x + width/2, y + height/2, 10, 10);
-    ellipse(x + width/2, y + height/2, 10, 10);
-    ellipse(x + width/2, y + height/2, 10, 10);
-  }
+  noLoop();
+  createCanvas(500, 500);
+}
+
+
+
 
 function draw() {
-  background(255,0,0);
-  text("p5.Polar Template", 100, height/2);
+  background(255, 0, 0);
+  text("p5.Polar Template", 100, height / 2);
+
   // Insert your drawing here
   // for loop - a piece of code
   // until the condition is met
-}
-  function drawing01() {
-    // Drawing logic can be added here.
-  
+  for (let i = 0; i < 360; i += 10) {
+    ellipse(i + width / 2, i + height / 2, 10, 10);
   }
 
-{drawing02()
-  for (j = 0; j < 360; j += 10)
-    ellipse(j + width/2, j + height/2, 10, 10);
-    ellipse(j + width/2, j + height/2, 10, 10);
-    ellipse(j + width/2, j + height/2, 10, 10);
+  // conditionals - if, else if, else
+  for (let j = 0; j < 360; j += 10) {
+    if (j % 2 === 0) {
+      fill(0, 0, 255);
+    } else {
+      fill(255, 255, 0);
+    }
+    ellipse(j + width / 2, j + height / 2, 10, 10);
   }
+}
+
+function drawing01() {
+  // Drawing logic can be added here.
+  strokeWeight(5);
+  for (let j = 0; j < 360; j += 10) {
+    // Drawing logic for the third sketch
+    ellipse(j + width / 2, j + height / 2, 10, 10);
+  }
+}
+
+function drawing02() {
+  for (let j = 0; j < 360; j += 10) {
+    if (j === 0) {
+      fill(255, 0, 0);
+    } else if (j < 180) {
+      fill(0, 255, 0);
+    } else {
+      fill(0, 0, 255);
+    }
+    ellipse(j + width / 2, j + height / 2, 10, 10);
+  }
+}
+
+
 
 
