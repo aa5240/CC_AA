38a87a0e5054c34c64762d2d085beb1c00ff03d7
aa5240 <aa5240@nyu.edu>
2026-09-16 19:05:37 -0400

    HW3 TEST

---
+++ week3/p5PolarTemplate/HW3_TEST.js
@@ -0,0 +1,34 @@
+// p5Polar Template
+// https://github.com/liz-peng/p5.Polar
+// https://liz-peng.github.io/p5.Polar/
+// Review the index.html for the <script></script> 
+// my first p5.Polar sketch
+function setup() {
+  noloop();
+  createCanvas(1000, 500);
+  for (let i = 0; i < 360; i += 10) 
+    ellipse(x + width/2, y + height/2, 10, 10);
+    ellipse(x + width/2, y + height/2, 10, 10);
+    ellipse(x + width/2, y + height/2, 10, 10);
+  }
+
+function draw() {
+  background(255,0,0);
+  text("p5.Polar Template", 100, height/2);
+  // Insert your drawing here
+  // for loop - a piece of code
+  // until the condition is met
+}
+  function drawing01() {
+    // Drawing logic can be added here.
+  
+  }
+
+{drawing02()
+  for (j = 0; j < 360; j += 10)
+    ellipse(j + width/2, j + height/2, 10, 10);
+    ellipse(j + width/2, j + height/2, 10, 10);
+    ellipse(j + width/2, j + height/2, 10, 10);
+  }
+
+
