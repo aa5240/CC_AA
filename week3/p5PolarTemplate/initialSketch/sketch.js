0e4e7e7a0374121789f25a411cb9ad2c30f1d634
aa5240 <aa5240@nyu.edu>
2026-09-23 15:19:57 -0400

    Update sketch.js

--- week3/p5PolarTemplate/initialSketch/sketch.js
+++ week3/p5PolarTemplate/initialSketch/sketch.js
@@ -2,10 +2,26 @@
 
 function setup() {
   createCanvas(500, 500);
+    r = random(10,100);
+  console.log(r);
+  
 }
 
 function draw() {
-  background(255,255,0);
-  text("Initial Sketch", 100, height/2);
+  background(r,100,0);
+   fill(255, 0, 0);
+  circle(mouseX, mouseY, 40);
   // Insert your drawing here
+  // Drawing logic can be added here.
+  strokeWeight(10);
+    const startingX = 50
+     const startingY = 50;
+    const s = r;
+     const space = 20;
+   for (let j = 0; j < 360; j += 10) 
+    ellipse(startingX + (j * space), height / 10, s);
+
+    ellipse(startingX + (i * space), startingY + j * space, s);
+    // First nested loop
 }
+ 
