e28e2997b0d98a14b507d79d2d99493d9590c3f9
aa5240 <aa5240@nyu.edu>
2026-09-23 18:01:17 -0400

    Shift

--- week3/p5PolarTemplate/p5PolarSketch/sketch.js
+++ week3/p5PolarTemplate/p5PolarSketch/sketch.js
@@ -5,10 +5,32 @@
 
 function setup() {
   createCanvas(500, 500);
-}
-
-function draw() {
-  background(255,0,0);
-  text("p5.Polar Template", 100, height/2);
   // Insert your drawing here
+  //size of shapes
+let s1= 200;//size of red circle
+let s2= 350;//size of green circle
+// below is an array of sizes for the rectangle, but I will just use one size for now
+let s3= [150,1000];//size of blue rectangle
+console.log(s3);
+  }
+
+function draw() {   
+  r = random(30,600);
+  console.log(r);
+  background(r, r, 255);
+  ellipse(r, r, 20, 20);
+ // My first nested for loop
+ fill (r,255,0)
+  strokeWeight(10);
+  const startingX = 20;
+  const startingY = 20;
+  const s = 20; // size
+  const space = r; // space in between center of shapes
+  for (i = 0; i < 5; i++) {
+    for (j = 0; j < 5; j++) {
+      polarEllipses(startingX + (i * space), startingY + j * space, s);
+      //console.log(j);
+
+}
+  }
 }
